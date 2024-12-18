9e714004becc89bd2e30d50f421f25b19ca420c8
Sekai02 <luisalejandroarteagamorales@gmail.com>
2024-12-17 23:33:49 -0500

    support for add, delete, modify txt files

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import threading
 
 
 def main():
-    local_addr = input("Enter the local Bluetooth address: ")
-    folder_path = input("Enter the folder path to sync: ")
-    peer_addr = input("Enter the Bluetooth address of the peer: ")
+    local_addr = input("Ingrese la dirección Bluetooth local: ")
+    folder_path = input("Ingrese la ruta de la carpeta a sincronizar: ")
+    peer_addr = input("Ingrese la dirección Bluetooth del par: ")
 
-    # Create a Bluetooth server instance
+    # Crear una instancia del servidor Bluetooth
     bt_server = server.BluetoothServer(local_addr, folder_path)
 
-    # Start the server in a separate thread
+    # Iniciar el servidor en un hilo separado
     server_thread = threading.Thread(target=bt_server.start_server, daemon=True)
     server_thread.start()
     print("Servidor Bluetooth iniciado.")
 
-    # Create a FolderSync instance and pass the peer address
+    # Crear una instancia de FolderSync y pasar la dirección del par
     sync_monitor = sync.FolderSync(folder_path, bt_server, peer_addr)
-    sync_monitor.start_sync()  # Start monitoring the folder
+    sync_monitor.start_sync()  # Iniciar la monitorización de la carpeta
 
 
 if __name__ == "__main__":
